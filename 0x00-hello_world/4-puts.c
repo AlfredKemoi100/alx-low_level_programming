65feef4286a872c7a840b7b287bc24bee221c810
AlfredKemoi100 <alfredkeep100@gmail.com>
2022-12-08 09:24:00 -0800

    C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-/*
+
+/**
  * main - print a code line using puts
  *
- * Return: 0
+ * Return: Always 0
  */
+
 int main(void)
 {	
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+
 }
